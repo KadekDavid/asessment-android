d7dada32c44532e0eae90aba404d3e12e437e201
Kadek David Kurniawan <davidkurniawan332@gmail.com>
2023-05-16 16:41:38 +0700

    menggunakan arsitektur MVVM

--- app/src/main/java/org/d3if0126/asessment/MainActivity.kt
+++ app/src/main/java/org/d3if0126/asessment/MainActivity.kt
@@ -3,22 +3,28 @@ package org.d3if0126.asessment
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
-import android.util.Log
 import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
 import org.d3if0126.asessment.databinding.ActivityMainBinding
+import org.d3if0126.asessment.model.HasilPersegiPanjang
+import org.d3if0126.asessment.model.MainViewModel
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private val viewModel: MainViewModel by lazy {
+        ViewModelProvider(this)[MainViewModel::class.java]
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.button.setOnClickListener { hitungLuas() }
+        binding.button.setOnClickListener { hitungPersegiPanjang() }
+        viewModel.getHasilPersegiPanjang().observe(this, { showResult(it) })
     }
-    private fun hitungLuas() {
+    private fun hitungPersegiPanjang() {
 
         val panjang = binding.panjangInp.text.toString()
         if (TextUtils.isEmpty(panjang)) {
@@ -30,9 +36,14 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this, R.string.lebar_invalid, Toast.LENGTH_LONG).show()
             return
         }
-
-        val luas = panjang.toInt() * lebar.toInt()
-
-        binding.LuasTextView.text = getString(R.string.luas_x, luas)
+            viewModel.hitungPersegiPanjang(
+            panjang.toInt(),
+            lebar.toInt()
+        )
+    }
+    private fun showResult(result: HasilPersegiPanjang?) {
+        if (result == null) return
+        binding.LuasTextView.text = getString(R.string.luas_x, result.luas)
+        binding.KelilingTextView.text = getString(R.string.keliling_x, result.keliling)
     }
 }
---
+++ app/src/main/java/org/d3if0126/asessment/model/HasilPersegiPanjang.kt
@@ -0,0 +1,6 @@
+package org.d3if0126.asessment.model
+
+data class HasilPersegiPanjang(
+    val luas : Int,
+    val keliling : Int
+)
