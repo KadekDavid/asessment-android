ed3efb0ddb4fb7a3adecd8759ae58d624738e220
Kadek David Kurniawan <davidkurniawan332@gmail.com>
2023-06-15 13:45:49 +0700

    assesment 3

--- app/src/main/java/org/d3if0126/asessment/MainActivity.kt
+++ app/src/main/java/org/d3if0126/asessment/MainActivity.kt
@@ -10,10 +10,12 @@ import android.util.Log
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
+import org.d3if0126.asessment.util.MyTimer
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
+    private lateinit var myTimer: MyTimer
     companion object {
         const val CHANNEL_ID = "updater"
         const val PERMISSION_REQUEST_CODE = 1
@@ -22,6 +24,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+        myTimer = MyTimer()
         Log.i("MainActivity", "onCreate dijalankan")
 
         navController = findNavController(R.id.myNavHostFragment)
@@ -44,6 +48,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onStart() {
         super.onStart()
+        myTimer.startTimer()
         Log.i("MainActivity", "onStart dijalankan")
     }
     override fun onResume() {
@@ -55,7 +60,8 @@ class MainActivity : AppCompatActivity() {
         super.onPause()
     }
     override fun onStop() {
-        Log.i("MainActivity", "onStop dijalankan")
+        Log.i("MainActivity", "onStop dipanggil.")
+        myTimer.stopTimer()
         super.onStop()
     }
     override fun onDestroy() {
